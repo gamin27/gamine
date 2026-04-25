abc1cb5631fbd9565db044d920248a770d8b8805
Yusuke Nagamine <nagamineyusuke17@gmail.com>
2026-04-25 10:55:37 +0900

    Update header navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
 
@@ -10,18 +10,6 @@ import vercel from '@astrojs/vercel';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
-
-	fonts: [
-		{
-			provider: fontProviders.google(),
-			name: 'Noto Serif JP',
-			cssVariable: '--font-noto-serif-jp',
-			weights: [400, 700],
-			styles: ['normal'],
-			subsets: ['japanese', 'latin'],
-			fallbacks: ['Hiragino Mincho ProN', 'Yu Mincho', 'serif'],
-		},
-	],
 
 	adapter: vercel(),
 });
